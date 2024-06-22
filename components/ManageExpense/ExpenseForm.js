9b5212142889340913a4e45048b34ff423c0b870
Prince Garg <43511492+PrinceGarg23@users.noreply.github.com>
2024-06-23 01:25:08 +0530

    input validation

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Alert} from 'react-native';
 
 import Input from './Input';
 import Button from '../UI/Button';
@@ -26,6 +26,15 @@ function ExpenseForm({ submitButtonLabel, onCancel, onSubmit, defaultValues }) {
       date: new Date(inputValues.date),
       description: inputValues.description
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+        Alert.alert('Invalid input', 'Please check your input values');
+        return;
+    }
 
     onSubmit(expenseData);
   }
